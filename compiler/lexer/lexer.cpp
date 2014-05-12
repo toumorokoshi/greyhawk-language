1e64d1611dc07aafb848564dc381b370af7468ce
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2014-05-11 19:48:03 -0700

    added single-character tokens

--- compiler/lexer/lexer.cpp
+++ compiler/lexer/lexer.cpp
@@ -8,8 +8,8 @@ using namespace std;
 
 namespace lexer {
 
-    // comparison operators
-    /*
+  // comparison operators
+  /*
     Token T_ASSIGN = Token("assign", "=");
     Token T_EQ = Token("equals", "==");
     Token T_NE = Token("not equals", "!=");
@@ -17,72 +17,109 @@ namespace lexer {
     Token T_LEQ = Token("less than or equal", "<=");
     Token T_GT = Token("greater than", ">");
     Token T_GEQ = Token("greater than or equal", ">=");
-    */
+  */
 
-    // keywords
-    Keyword* T_ELSE = new Keyword("else");
-    Keyword* T_IF = new Keyword("if");
-    Keyword* T_IS = new Keyword("is");
-    Keyword* T_RETURN = new Keyword("return");
+  // keywords
+  Keyword* T_ELSE = new Keyword("else");
+  Keyword* T_IF = new Keyword("if");
+  Keyword* T_IS = new Keyword("is");
+  Keyword* T_RETURN = new Keyword("return");
 
-    // constants
-    Keyword* T_FALSE = new Keyword("false");
-    Keyword* T_TRUE = new Keyword("true");
+  // constants
+  Keyword* T_FALSE = new Keyword("false");
+  Keyword* T_TRUE = new Keyword("true");
 
-   // boolean operations
+  // single character, non-alphanumeric characters
+  Character* T_LPAREN = new Character('(');
+  Character* T_RPAREN = new Character(')');
+  Character* T_DOT = new Character('.');
+  Character* T_COMMA = new Character(',');
+  Character* T_PLUS = new Character('+');
+  Character* T_MINUS = new Character('-');
+  Character* T_MUL = new Character('*');
+  Character* T_DIV = new Character('/');
 
-    KeywordVector keywordList {
+  KeywordVector keywordList {
       T_ELSE,
-        T_IF,
-        T_IS,
-        T_RETURN,
-        T_FALSE,
-        T_TRUE
-    };
+      T_IF,
+      T_IS,
+      T_RETURN,
+      T_FALSE,
+      T_TRUE
+  };
 
-    bool isAlpha(char c) {
-      if ('a' <= c && c <= 'z') {
-        return true;
-      } else if ('A' <= c && c <= 'Z') {
-        return true;
-      }
-      return false;
+  CharacterVector characterList {
+      T_LPAREN,
+      T_RPAREN,
+      T_DOT,
+      T_COMMA,
+      T_PLUS,
+      T_MINUS,
+      T_MUL,
+      T_DIV
+  };
+
+  bool isAlpha(char c) {
+    if ('a' <= c && c <= 'z') {
+      return true;
+    } else if ('A' <= c && c <= 'Z') {
+      return true;
     }
+    return false;
+  }
 
-    bool isNumeric(char c) {
-      return ('0' <= c && c <= '9');
-    }
+  bool isNumeric(char c) {
+    return ('0' <= c && c <= '9');
+  }
 
-    bool isAlphaNumeric(char c) {
-      return isAlpha(c) || isNumeric(c);
-    }
+  bool isAlphaNumeric(char c) {
+    return isAlpha(c) || isNumeric(c);
+  }
 
-    TokenVector tokenize(string input) {
 
-      TokenVector tokens;
+  TokenVector tokenize(string input) {
 
-      string current_token = "";
+    TokenVector tokens;
 
-      for (char& c : input) {
-        if (!isAlphaNumeric(c)) {
-          throw LexerException("Can not handle non-alphanumeric characters!");
-        } else {
+    string current_token = "";
 
-          current_token += c;
-          for (KeywordVector::iterator it = keywordList.begin(); it != keywordList.end(); ++it) {
-            if (current_token.compare((*it)->symbol) == 0) {
-              current_token = "";
-              tokens.push_back(*it);
-            }
-          }
-
+    for (char& c : input) {
+      if (!isAlphaNumeric(c)) {
+        // create an identifier token
+        if (current_token.compare("") != 0) {
+          tokens.push_back(new Identifier(current_token));
+          current_token = "";
         }
-      }
 
-      if (current_token.compare("") != 0) {
-        throw LexerException("invalid token: " + current_token);
+        bool matching_token = false;
+        for (CharacterVector::iterator it = characterList.begin(); it != characterList.end(); ++it) {
+          if ((*it)->symbol == c) {
+            matching_token = true;
+            tokens.push_back(*it);
+          }
+        }
+
+        if (!matching_token) {
+          throw LexerException("Unable to find token matching " + string(1, c));
+        }
+
+      } else {
+
+        current_token += c;
+        for (KeywordVector::iterator it = keywordList.begin(); it != keywordList.end(); ++it) {
+          if (current_token.compare((*it)->symbol) == 0) {
+            current_token = "";
+            tokens.push_back(*it);
+          }
+        }
+
       }
-      return tokens;
     }
+
+    if (current_token.compare("") != 0) {
+      throw LexerException("invalid token: " + current_token);
+    }
+    return tokens;
+  }
 
 }
--- compiler/lexer/lexer.hpp
+++ compiler/lexer/lexer.hpp
@@ -8,37 +8,50 @@
 
 namespace lexer {
 
-    // extend exceptions from here if they have to do with
-    // the lexer specifically
-    class LexerException: public std::exception {
-    public:
-      const std::string message;
-      LexerException(std::string _message) : message(_message) {}
-    };
+  // extend exceptions from here if they have to do with
+  // the lexer specifically
+  class LexerException: public std::exception {
+  public:
+    const std::string message;
+    LexerException(std::string _message) : message(_message) {}
+  };
 
-    class Token {
-    public:
-      virtual std::string getDescription() { return "nothing"; }
-    };
+  class Token {
+  public:
+    virtual std::string getDescription() { return "nothing"; }
+  };
 
+  class Character : public Token {
+  public:
+    const char symbol;
+    Character(char _symbol) : symbol(_symbol) {};
+    std::string getDescription() { return std::string(1, symbol); }
+  };
 
-    class Keyword : public Token {
-    public:
-      const std::string symbol;
-      Keyword(std::string _symbol) : symbol(_symbol) {}
-      std::string getDescription() { return symbol; }
-    };
+  class Keyword : public Token {
+  public:
+    const std::string symbol;
+    Keyword(std::string _symbol) : symbol(_symbol) {}
+    std::string getDescription() { return "keyword: " + symbol; }
+  };
 
+  class Identifier : public Token {
+  public:
+    const std::string name;
+    Identifier(std::string _name) : name(_name) {}
+    std::string getDescription() { return "identifier: " + name; }
+  };
 
-    typedef std::vector<Token*> TokenVector;
-    typedef std::vector<Keyword*> KeywordVector;
+  typedef std::vector<Token*> TokenVector;
+  typedef std::vector<Keyword*> KeywordVector;
+  typedef std::vector<Character*> CharacterVector;
 
-    TokenVector tokenize(std::string input);
+  TokenVector tokenize(std::string input);
 
-    // utility methods
-    bool isAlpha(char);
-    bool isNumeric(char);
-    bool isAlphaNumeric(char);
+  // utility methods
+  bool isAlpha(char);
+  bool isNumeric(char);
+  bool isAlphaNumeric(char);
 
 }
 
