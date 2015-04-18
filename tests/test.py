c227efafda450b02615f3a4d7e36036794780964
Yusuke Tsutsumi <tsutsumi.yusuke@gmail.com>
2015-04-18 08:08:14 -0700

    adding full tests

--- tests/test.py
+++ tests/test.py
@@ -25,13 +25,6 @@ RESULT_OUTPUT_DIR = os.path.join(TEST_DIR, 'result')
 OUTPUT_DOESNT_EXIST = "Output for {type} {file} doesn't exist! do you want to create it? (default no) "
 OUTPUT_CHANGED = "Output changed! accept changes? (default no) "
 
-
-def get_yes_or_no(prompt):
-    """ convert user input to a yes or no """
-    response = input(prompt)
-    cleaned_response = response.strip().lower()
-    return len(cleaned_response) > 0 and cleaned_response[0] in ('y', 't')
-
 TEST_FILE_TYPES = {
     "ast": {
         "extra_params": "--ast",
@@ -45,52 +38,69 @@ TEST_FILE_TYPES = {
     }
 }
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-p", "--prompt", help="Instead of failing, prompt to see if content should be overwritten", action="store_true")
-args = parser.parse_args()
 
-errors = []
-tests = 0
-for f in filter(lambda x: x.endswith('gh'), os.listdir(EXAMPLES_DIRECTORY)):
-    tests += 1
-    target_file_path = os.path.join(EXAMPLES_DIRECTORY, f)
-    # check if ast matches
+def _test_file(target_file, extra_params):
+    command = shlex.split('{0} {1} {2}'.format(
+        EXECUTABLE, target_file, extra_params
+    ))
+    child = subprocess.Popen(command, stdout=subprocess.PIPE)
+    stdout, stderr = child.communicate()
+    if child.returncode != 0:
+        print("  executing {0} with {1} had exit code {2}".format(
+            target_file, extra_params, child.returncode
+        ))
+        return None
+    return stdout
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--prompt", help="Instead of failing, prompt to see if content should be overwritten", action="store_true")
+    args = parser.parse_args()
+
+    tests = 0
+    successes = 0
     for test_type, test_file_type in TEST_FILE_TYPES.items():
-        command = shlex.split('{0} {1} {2}'.format(
-            EXECUTABLE, target_file_path, test_file_type['extra_params'])
-        )
-        output = subprocess.call(command, stderr=subprocess.STDOUT).decode('utf-8')
-        output_file = os.path.join(test_file_type['test_directory'], f)
-        overwrite = False
-        if not os.path.exists(output_file):
-            if args.prompt:
-                overwrite = get_yes_or_no(OUTPUT_DOESNT_EXIST.format(type=test_type,
-                                                                     file=output_file))
-        else:
-            with open(output_file, 'r') as fh:
-                desired_output = fh.read()
-                if output.strip() != desired_output.strip():
-                    for line in difflib.context_diff(output.split('\n'), desired_output.split('\n'),
-                                                     "Current Output", "Desired Output"):
-                        print(line)
-                    overwrite = get_yes_or_no(OUTPUT_CHANGED) if args.prompt else False
-                else:
-                    continue
-        if overwrite:
-            if not os.path.exists(os.path.dirname(output_file)):
-                os.makedirs(os.path.dirname(output_file))
-            with open(output_file, 'w+') as fh:
-                fh.write(output)
-        else:
-            errors.append((f, test_type, output))
+        print(test_type + ":")
 
-if errors:
-    print("Errors were encountered!")
-    for f, test_type, output in errors:
-        print("{0} with {1}:".format(f, test_type))
-        print("#" * 40)
-        print(output)
-    print("{0} tests failed!".format(len(errors)))
-    exit(1)
-else:
-    print("{0} files passed!".format(tests))
+        for name in os.listdir(EXAMPLES_DIRECTORY):
+            if not name.endswith('.gh'):
+                continue
+
+            tests += 1
+            target_file_path = os.path.join(EXAMPLES_DIRECTORY, name)
+
+            output = _test_file(target_file_path, test_file_type['extra_params'])
+
+            if not output:
+                continue
+
+            output = output.decode('utf-8')
+
+            output_file = os.path.join(test_file_type['test_directory'], name)
+
+            if not os.path.exists(output_file):
+                print("  output file not found for {0}. creating...".format(
+                    name
+                ))
+                with open(output_file, 'w+') as fh:
+                    fh.write(output)
+
+            else:
+                with open(output_file, 'r') as fh:
+                    desired_output = fh.read()
+                    if output.strip() != desired_output.strip():
+                        print("  output file {0} has modified output:".format(name))
+                        for line in difflib.context_diff(
+                                output.split('\n'), desired_output.split('\n'),
+                                "Current Output", "Desired Output"
+                        ):
+                            print(line)
+
+            successes += 1
+
+    print("{0}/{1} files passed!".format(successes, tests))
+
+
+main()
